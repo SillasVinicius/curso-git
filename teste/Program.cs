a440b5653c810e89ef1539c1d28fc374c24daffd
Sillas Vinícius <sillasvinicius0403@gmail.com>
2018-02-05 09:15:02 -0200

    Acrescentando a mensagem desejando um bom dia

--- teste/Program.cs
+++ teste/Program.cs
@@ -11,8 +11,8 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
